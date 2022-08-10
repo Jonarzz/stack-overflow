34c4385576ddabbbbaa6e109fcc3851040fabb14
Jonarzz <rajek7@gmail.com>
2022-08-10 07:42:56 +0200

    Added a Spring Boot module to faster answer related questions

---
+++ spring-boot-module/src/test/java/io/github/jonarzz/SpringBootModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonarzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
